f7fbf4f093ddd5ac2388da20feca19397f5cd6ed
Tyler Small <email": null>
2017-02-14 12:08:52 -0600

    Update migrations to reflect required data

--- db/migrate/20170214002456_create_movies.rb
+++ db/migrate/20170214002456_create_movies.rb
@@ -1,7 +1,7 @@
 class CreateMovies < ActiveRecord::Migration
   def change
     create_table :movies do |t|
-      t.string :title
+      t.string :title, null: false
       t.text   :description
 
       t.timestamps null: false
