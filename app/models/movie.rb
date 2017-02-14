762a35ea6a25a6b57635c304cb4bc9816a9ae5e7
Tyler Small <intrasolarmusic@gmail.com>
2017-02-14 15:31:25 -0600

    Associations (#2)
    * Setup RSpec for model testing
    
    * Bring in Faker for test data. Begin TDD of associations
    
    * Update specs to remove redundant tests.
    
    * Re-bundle and install rspec. Gemlock changed from windows dependencies
    
    * Update DB Schema to add null constraints. Write tests to document attributes/associations
    
    * Update migrations to reflect required data
    
    * Update tests to account for validations
    
    * Add validations in models
    
    * Update Schema with DB-level validations
    
    * Write temp tests to ensure working rails_helper
    
    * Add factory girl for testing
    
    * Bring in factory girl to rails_helper

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -1,2 +1,4 @@
 class Movie < ActiveRecord::Base
+  has_many :showtimes
+  validates :title, :description, presence: true
 end
---
+++ spec/models/screen_spec.rb
@@ -0,0 +1,17 @@
+require 'rails_helper'
+
+describe Screen do
+  describe "Associations" do
+    it "belongs to a movie" do
+
+    end
+  end
+
+  it "has a passing test" do
+    expect(true)
+  end
+
+  it "has a failing test" do
+    expect(true).to be false
+  end
+end
