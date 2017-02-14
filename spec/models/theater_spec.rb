489e357ec9bffac972d9b2140088cb69dd77fd70
Tyler Small <email": null>
2017-02-14 12:29:48 -0600

    Update tests to account for validations

--- spec/models/theater_spec.rb
+++ spec/models/theater_spec.rb
@@ -5,13 +5,6 @@ describe Theater do
   let(:name){ Faker::Company.name }
 	let(:t) { Theater.create!(address: address, name: name) }
 
-  describe "associations" do
-		it "has many screens" do
-			4.times { t.screens << Screen.new }
-			expect(t.screens.length).to be 4
-		end
-	end
-
   describe "attributes" do
     it "has a readable name and address" do
       expect(t.name).to eq name
