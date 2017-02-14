3ca3e8b06674c68581bf6a34f38461588420c003
tylersmall19 <tylersmall.user@gmail.com>
2017-02-13 19:39:03 -0600

    Bring in Faker for test data. Begin TDD of associations

--- spec/models/theater_spec.rb
+++ spec/models/theater_spec.rb
@@ -1,6 +1,10 @@
 require 'rails_helper'
+
 describe Theater do
-	before do
-	  
+	context "When valid data is given" do
+		it "persists to the data base" do
+			t = Theater.create!(address: )
+			expect(t.persisted?)
+		end
 	end
 end
