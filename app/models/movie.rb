312d46fc0478111e2ccd8e981e9b503e8d0a45aa
Tyler Small <email": null>
2017-02-14 12:30:32 -0600

    Add validations in models

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -1,3 +1,4 @@
 class Movie < ActiveRecord::Base
   has_many :showtimes
+  validates :title, :description, presence: true
 end
--- app/models/theater.rb
+++ app/models/theater.rb
@@ -1,4 +1,4 @@
 class Theater < ActiveRecord::Base
 	has_many :screens
-	validates :address, presence: true
+	validates :address, :name, presence: true
 end
