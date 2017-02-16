abaeef4d7f7019c987b69411ac114b6571aa439f
Tyler Small <email": null>
2017-02-16 16:13:25 -0600

    Update model, row display and show page.

--- app/models/showtime.rb
+++ app/models/showtime.rb
@@ -1,12 +1,16 @@
 class Showtime < ActiveRecord::Base
   belongs_to :movie
   belongs_to :screen
-  has_many :tickets
   has_many :orders
+  has_many :tickets, through: :orders
 
   validates :time, :movie, :screen, presence: true
 
   def pretty_time
     time.getlocal.strftime("%I:%M %p")
+  end
+
+  def tickets_left
+    screen.capacity - orders.length
   end
 end
