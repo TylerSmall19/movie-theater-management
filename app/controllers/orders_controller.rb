5157b7ef010dc3e94484bd4ae626e631b371265d
Tyler Small <email": null>
2017-02-16 11:15:53 -0600

    Update new order form and controller

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -2,4 +2,21 @@ class OrdersController < ApplicationController
   def new
     @showtime=Showtime.find_by(id: params[:showtime_id])
   end
+
+  def create
+    order = Order.new(order_params)
+    redirect_to root if order.valid?
+    redirect_back
+  end
+
+  private
+  def order_params
+    params.require(:order).permit(
+                            :email,
+                            :name,
+                            :credit_card,
+                            :expiration,
+                            :showtime
+                          )
+  end
 end
