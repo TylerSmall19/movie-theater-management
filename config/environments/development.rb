db006c5fff7bd812afce2aaf0bc5aa6fe313ea6d
Tyler Small <email": null>
2017-02-17 17:36:35 -0600

    Enable template location of order_mailer. Setup gmail as my SMTP server.

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -38,4 +38,22 @@ Rails.application.configure do
 
   # Raises error for missing translations
   # config.action_view.raise_on_missing_translations = true
+
+  #Added per active admin install instructions
+  config.action_mailer.default_url_options = { :host => 'localhost:3000' }
+
+  #These settings are for the sending out email for active admin and consequently the   devise mailer
+  ActionMailer::Base.delivery_method = :smtp
+  ActionMailer::Base.perform_deliveries = true
+  ActionMailer::Base.raise_delivery_errors = true
+  ActionMailer::Base.smtp_settings =
+  {
+
+    :address            => 'smtp.gmail.com',
+    :port               => 587,
+    :domain             => 'gmail.com', #you can also use google.com
+    :authentication     => :plain,
+    :user_name          => ENV["EMAIL_USERNAME"],
+    :password           => ENV["EMAIL_PASSWORD"]
+  }
 end
