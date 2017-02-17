586bf322a57958d436ebad674a7d0c5ed7b432d2
Tyler Small <email": null>
2017-02-16 21:20:59 -0600

    Document changes in comments

--- app/models/order.rb
+++ app/models/order.rb
@@ -23,6 +23,7 @@ class Order < ActiveRecord::Base
 
   private
   def tickets_are_available
+    # Safe guard against errors when no showtime is set
     errors.add :tickets, "are sold out for this showtime." unless showtime && showtime.tickets_left > 0
   end
 end
