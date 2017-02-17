15fe4ce06e344cf6bdaf0c1952a2a14a35fe3d46
Tyler Small <intrasolarmusic@gmail.com>
2017-02-16 18:00:59 -0600

    Disable ticket purchases if capacity is reached. (#10)

--- app/models/order.rb
+++ app/models/order.rb
@@ -9,11 +9,18 @@ class Order < ActiveRecord::Base
             :showtime,
             { presence: true }
 
+  validate :tickets_are_available
+
   def last_four_of_card
     credit_card[-4..-1]
   end
 
   def total
     showtime.price
+  end
+
+  private
+  def tickets_are_available
+    errors.add :tickets, "are sold out for this showtime." unless showtime.tickets_left > 0
   end
 end
