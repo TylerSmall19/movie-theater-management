84a2e51197e77a0a3e3fe1c8e6d57156f82c6fa4
Tyler Small <email": null>
2017-02-18 15:50:12 -0600

    Add view, controller method, redirect for new showtimes

--- app/controllers/showtimes_controller.rb
+++ app/controllers/showtimes_controller.rb
@@ -2,4 +2,8 @@ class ShowtimesController < ApplicationController
   def index
     @showtimes = Movie.find_by(id: params[:movie_id]).showtimes
   end
+
+  def new
+    @movie = Movie.find_by(id: params[:movie_id])
+  end
 end
