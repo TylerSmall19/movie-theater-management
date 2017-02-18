301753c7242da0dd7c63fe1e52c56f2d63989add
Tyler Small <email": null>
2017-02-18 15:29:00 -0600

    Add filter helper method to pull movies

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -1,4 +1,8 @@
 class Movie < ActiveRecord::Base
   has_many :showtimes
   validates :title, :description, presence: true
+
+  def self.all_with_showtimes
+    Movie.all.select { |movie| movie.showtimes.any? }
+  end
 end
