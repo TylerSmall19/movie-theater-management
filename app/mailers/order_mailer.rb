9adf0f3d6fea7b31c1722e0db32797c85e392121
Tyler Small <email": null>
2017-02-17 18:09:30 -0600

    Update email templates

--- app/mailers/order_mailer.rb
+++ app/mailers/order_mailer.rb
@@ -2,6 +2,9 @@ class OrderMailer < ApplicationMailer
   def confirmation(order)
     @order = order
     mail_subject = "Order Confirmation for #{@order.name}"
+    p '*' * 80
+    p ENV["GMAIL_USERNAME"]
+    p '*' * 80
     mail(to: @order.email, subject: mail_subject)
   end
 end
