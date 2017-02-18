d2f039c2d8f50a741b76cbc99557dd7ceaf0c984
Tyler Small <email": null>
2017-02-18 16:32:25 -0600

    Allow showtime creation for movies

--- app/controllers/showtimes_controller.rb
+++ app/controllers/showtimes_controller.rb
@@ -6,4 +6,22 @@ class ShowtimesController < ApplicationController
   def new
     @movie = Movie.find_by(id: params[:movie_id])
   end
+
+  def create
+    @movie = Movie.find_by(id: params[:movie_id])
+    @showtime = @movie.showtimes.build(showtime_params)
+
+    if @showtime.save
+      redirect_to movie_showtime_path(@movie, @showtime)
+    else
+      p @errors = @showtime.errors.full_messages
+      render :new
+    end
+  end
+
+  private
+
+  def showtime_params
+    params.require(:showtime).permit(:screen_id, :time, :price)
+  end
 end
