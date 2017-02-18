16343c588e4d43b3288ff28418857fa02d27e75c
Tyler Small <email": null>
2017-02-17 18:22:08 -0600

    Update specs to show TODO tasks

--- spec/models/order_spec.rb
+++ spec/models/order_spec.rb
@@ -11,4 +11,12 @@ describe Order do
     it { is_expected.to belong_to :showtime }
     it { is_expected.to have_many :tickets }
   end
+
+  describe "TODO" do
+    pending "Allow Movie/Showtime additions"
+    pending "Tracking sales"
+    pending "Bootstrap/CSS"
+    pending "TODO Comments"
+    pending "ReadMe"
+  end
 end
