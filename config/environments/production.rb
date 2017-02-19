cc78fb2425c615b046e94037d70544a577d9aa86
Tyler Small <email": null>
2017-02-18 19:20:27 -0600

    Copy over email config to production. Remove TODO from dev environment

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -76,4 +76,21 @@ Rails.application.configure do
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
+
+  # Configure action_mailer
+  config.action_mailer.default_url_options = { :host => 'localhost:3000' }
+
+  # These settings are for the sending out email for active admin and consequently the devise mailer
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.perform_deliveries = true
+  config.action_mailer.raise_delivery_errors = true
+  config.action_mailer.smtp_settings =
+  {
+    :address            => 'smtp.gmail.com',
+    :port               => 587,
+    :domain             => 'gmail.com', #you can also use google.com
+    :authentication     => :plain,
+    :user_name          => ENV["GMAIL_USERNAME"],
+    :password           => ENV["GMAIL_PASSWORD"]
+  }
 end
