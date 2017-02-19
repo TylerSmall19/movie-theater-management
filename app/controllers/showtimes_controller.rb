57b74f5f8784e83af0eb382dced6742ffaf05ab8
Tyler Small <email": null>
2017-02-18 19:48:50 -0600

    Refactor controller to avoid uneeded instance variables

--- app/controllers/showtimes_controller.rb
+++ app/controllers/showtimes_controller.rb
@@ -12,12 +12,23 @@ class ShowtimesController < ApplicationController
     @showtime = Showtime.find_by(id: params[:id])
   end
 
+  def update
+    @showtime = Showtime.find_by(id: params[:id])
+    @showtime.update_attributes(showtime_params)
+    if @showtime.save
+      redirect_to movie_showtimes_path(@showtime.movie)
+    else
+      @errors = @showtime.errors.full_messages
+      render :edit
+    end
+  end
+
   def create
-    @movie = Movie.find_by(id: params[:movie_id])
-    @showtime = @movie.showtimes.build(showtime_params)
+    movie = Movie.find_by(id: params[:movie_id])
+    @showtime = movie.showtimes.build(showtime_params)
 
     if @showtime.save
-      redirect_to movie_showtimes_path(@movie)
+      redirect_to movie_showtimes_path(movie)
     else
       p @errors = @showtime.errors.full_messages
       render :new
