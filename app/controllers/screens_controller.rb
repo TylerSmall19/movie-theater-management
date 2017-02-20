be89280e3ef304f26678abc43a597179899b79d2
Tyler Small <intrasolarmusic@gmail.com>
2017-02-20 14:30:19 -0600

    Edit screens (#21)
    * Enable screen editing
    
    * Update model, specs, controller to destory screens and dependents
    
    * Fix indentation error
    
    * Remove trailing whitespace

--- app/controllers/screens_controller.rb
+++ app/controllers/screens_controller.rb
@@ -4,39 +4,48 @@ class ScreensController < ApplicationController
   def show; end
 
   def index
-  	@screens = Screen.order(:number)
+    @screens = Screen.order(:number)
   end
 
   def edit
-  	@screen = Screen.find_by(id: params[:id])
+    @screen = Screen.find_by(id: params[:id])
   end
 
   def create
-  	@screen = Screen.new(screen_params)
+    @screen = Screen.new(screen_params)
 
-  	if @screen.save
-  		redirect_to @screen
-  	else
-  		@errors = @screen.errors.full_messages
-  		render :new
-  	end
+    if @screen.save
+      redirect_to @screen
+    else
+      @errors = @screen.errors.full_messages
+      render :new
+    end
   end
 
   def update
-  	@screen = Screen.find_by(id: params[:id])
-  	@screen.update_attributes(screen_params)
+    @screen = Screen.find_by(id: params[:id])
+    @screen.update_attributes(screen_params)
 
-  	if @screen.save
-  		redirect_to @screen
-  	else
-  		@errors = @screen.errors.full_messages
-  		render :edit
-  	end
+    if @screen.save
+      redirect_to screens_path
+    else
+      @errors = @screen.errors.full_messages
+      render :edit
+    end
+  end
+
+  def destroy
+    Screen.find_by(id: params[:id]).destroy
+    redirect_to screens_path
   end
 
   private
 
   def screen_params
-  	params.require(:screen).permit(:number, :capacity, :description)
+    params.require(:screen)
+          .permit(:number,
+                  :capacity,
+                  :description,
+                  :movie_id)
   end
 end
