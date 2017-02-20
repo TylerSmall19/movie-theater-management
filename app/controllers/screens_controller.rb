f3268420513a5a740b9fcfe81feec3f8aa32b4e2
Tyler Small <intrasolarmusic@gmail.com>
2017-02-20 13:21:59 -0600

    Update controller to add new screens.

--- app/controllers/screens_controller.rb
+++ app/controllers/screens_controller.rb
@@ -1,13 +1,26 @@
 class ScreensController < ApplicationController
-  def new
+  def new; end
+
+  def show; end
+
+  def index; end
+
+  def edit; end
+
+  def create
+  	@screen = Screen.new(screen_params)
+
+  	if @screen.save
+  		redirect_to @screen
+  	else
+  		@errors = @screen.errors.full_messages
+  		render :new
+  	end
   end
 
-  def show
-  end
+  private
 
-  def index
-  end
-
-  def edit
+  def screen_params
+  	params.require(:screen).permit(:number, :capacity, :description)
   end
 end
