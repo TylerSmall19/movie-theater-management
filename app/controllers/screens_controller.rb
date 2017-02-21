cded72b419cd2e8b35e2afa96545ec200312efb9
Tyler Small <intrasolarmusic@gmail.com>
2017-02-20 20:21:46 -0600

    Bootstrap orders (#26)
    * Style index page. Refactor index into bootstrap_form_for
    
    * Add parens to keep with application conventions
    
    * Update orders index to display in tables
    
    * Style orders form and add validations with bootstrap form_for
    
    * Begin cleaning up form partial calls after bootstrap refactor
    
    * Refactor all Bootstrap_forms to remove @errors

--- app/controllers/screens_controller.rb
+++ app/controllers/screens_controller.rb
@@ -1,5 +1,7 @@
 class ScreensController < ApplicationController
-  def new; end
+  def new
+    @screen = Screen.new
+  end
 
   def index
     @screens = Screen.order(:number)
@@ -15,7 +17,6 @@ class ScreensController < ApplicationController
     if @screen.save
       redirect_to @screen
     else
-      @errors = @screen.errors.full_messages
       render :new
     end
   end
@@ -27,7 +28,6 @@ class ScreensController < ApplicationController
     if @screen.save
       redirect_to screens_path
     else
-      @errors = @screen.errors.full_messages
       render :edit
     end
   end
